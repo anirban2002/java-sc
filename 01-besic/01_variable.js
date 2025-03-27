ed0cbdf075a253cacfcbe096a9993437b8259e61
anirban2002 <pupunparya2002@gmail.com>
2025-03-27 20:47:16 +0530

    day-1

---
+++ 01-besic/01_variable.js
@@ -0,0 +1,9 @@
+const accountId="20P31A0502"
+let accountEmail="pupunparya2002@gmail.com"
+var accountPassword="12345"
+accountCity="jaypur"
+accountEmail="ani2002@gmail.com"
+accountPassword="665432"
+accountCity="bamanda"
+console.table([accountEmail,accountId,accountPassword,accountCity])
+console.log(accountId);
