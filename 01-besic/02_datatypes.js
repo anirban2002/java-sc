8366fb47a3d1b4917bcd02da9cd99f6224012d0a
anirban2002 <pupunparya2002@gmail.com>
2025-04-06 17:42:47 +0530

    02_datatupes

---
+++ 01-besic/02_datatypes.js
@@ -0,0 +1,16 @@
+let name="anirban"
+let age=23
+let  newLocal = false
+console.log(typeof age);
+console.log(typeof null);
+//few datatypes
+//number=>2 to power 53
+//bigint=>using in stock market
+//string=>" "
+//boolean=>true/false
+//null=>standlone value
+//undefined=>
+//symbol=>unique
+
+
+//object
