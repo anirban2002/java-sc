0924b9135d8c42d926224dbfab0982749e4addb6
anirban2002 <pupunparya2002@gmail.com>
2025-04-07 10:49:18 +0530

    03_conversionOperation

---
+++ 01-besic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let number=null
+console.log(typeof number)
+console.log(number)
+let valueis=Number(number)
+console.log(valueis)
+//"33"=>33
+//"33abc"=>NaN
+//true=1 & false=0
+let isLogged=1
+let booleanIsLogged=Boolean(isLogged)
+console.log(booleanIsLogged)
+//1=>true
+//""=>false
+//"anirban"=>true
+let somenumber=30
+let stringnumber=String(somenumber)
+console.log(somenumber);
+console.log(typeof stringnumber);
