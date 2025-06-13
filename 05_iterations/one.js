82b7387499217bf6c143ff5537f450a3bf385b33
anirban2002 <pupunparya2002@gmail.com>
2025-06-13 20:38:29 +0530

    one.js

---
+++ 05_iterations/one.js
@@ -0,0 +1,23 @@
+//for
+for (let i=0;i<=10;i++){
+    const element=i;
+    if(element==5){
+        //console.log("5 is best number");
+}
+//console.log(element);
+}
+//
+for (let i = 0; i < 10; i++) {
+    //console.log(`Outer loop value:${i}`)
+    for (let j = 0; j < 10; j++) {
+    //console.log(`Inner loop value ${j} and Inner loop value ${i}`);
+    //console.log(i+ '*' + j + '=' +i*j);
+    }
+}
+let myArray=["Flash","batman","superman"]
+console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
