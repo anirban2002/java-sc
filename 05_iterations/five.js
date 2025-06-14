df8743dd1f5f7997082f1f5f21b5aaf1efbfc3d6
anirban2002 <pupunparya2002@gmail.com>
2025-06-14 20:06:17 +0530

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,10 @@
+const coding =["js","rb","py","java"]
+coding.forEach(function(item) {
+    console.log(item)
+})
+coding.forEach((item)=>{
+    console.log(item)
+})
+function printMe(item){
+    console.log(item);
+}
